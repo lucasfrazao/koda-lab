3c7123f51f8775b531cd7bd29da9a2be3de63348
Lucas Frazão <lucasfraza0@outlook.com>
2024-06-15 02:09:27 -0300

    feat: install and configuration tailwind and shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+import "./global.css";
 export function App() {
   return (
     <>
       <h1>KodaLab</h1>
+      <Button>Click me</Button>
     </>
   );
 }
